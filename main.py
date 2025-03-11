375e240f5cd9ccf7e68525f8fc4c3c4d7637ad86
HesedAlejandro <vk0930@hotmail.com>
2025-03-10 22:38:59 -0600

    firstcommit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Importar la función de entrenamiento y evaluación desde el módulo neural_networks_keras
+from src.binary_Classification_NLP import train_imdb_model
+
+# Verifica si el script se ejecuta directamente
+if __name__ == "__main__":
+   train_imdb_model()  # Llama a la función para entrenar y evaluar la red neuronal
